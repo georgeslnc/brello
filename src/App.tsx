a2383a534c0295a1b834be696b2458866d7c3964
George Slansky <g.slanskii@21-school.ru>
2025-02-02 15:36:31 +0300

    fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { useState } from "react";
-import viteLogo from "/vite.svg";
 
 import "./App.css";
-
-
-
-
 import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -28,8 +24,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p 
-      className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
